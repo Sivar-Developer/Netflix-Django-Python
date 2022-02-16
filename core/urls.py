75961fc397d8c2514b745fd6b43b3b95d0bd56c8
Sivar-Developer <fragmatica.dc@gmail.com>
2022-02-16 11:35:07 +0300

    Profiles list

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
+from unicodedata import name
 from django.urls import path
-
-from core.views import Home
-from .views import Home
+from .views import Home, ProfileList
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('profile/', ProfileList.as_view(), name="profile_list")
 ]
