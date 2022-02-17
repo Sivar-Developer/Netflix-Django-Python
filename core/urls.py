15d0edd622525bbb06c00aafba8cef7a91d1ae70
Sivar-Developer <fragmatica.dc@gmail.com>
2022-02-17 10:55:11 +0300

    Profile create view

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import Home, ProfileList
+from .views import Home, ProfileList, ProfileCreate
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view()),
-    path('profile/', ProfileList.as_view(), name="profile_list")
+    path('profile/', ProfileList.as_view(), name="profile_list"),
+    path('profile/create/', ProfileCreate.as_view(), name="profile_create"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from core.models import Profile
+from .forms import ProfileForm
+
 # Create your views here.
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
         return render(request, 'index.html')
     
 @method_decorator(login_required, name='dispatch')
@@ -14,4 +19,26 @@ class ProfileList(View):
         profiles = request.user.profiles.all()
         return render(request, 'profileList.html', {
             'profiles': profiles
+        })
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        # form for creating profile
+        form = ProfileForm()
+        return render(request, 'profileCreate.html', {
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        
+        if form.is_valid():
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('core:profile_list')
+        
+        return render(request, 'profileCreate.html', {
+            'form': form
         })
